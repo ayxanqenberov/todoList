0c5546a755096e956b2a6bb9c059af2fd69c3384
098100ayxan <ganbarov008@gmail.com>
2024-08-29 16:22:00 +0400

    done

--- todo.js
+++ todo.js
@@ -3,62 +3,145 @@ const todoAdding = document.querySelector("#todoName")
 const listgroup = document.querySelector(".list-group")
 const searching = document.querySelector("#todoSearch")
 addForm.addEventListener("submit", getAdd)
-let data = []
+
+// bir todo elave etmek ucun
 function getAdd(e) {
   e.preventDefault()
-  data.push(todoAdding.value)
-  localStorage.setItem('tasks',JSON.stringify(data))
-  addingOnList(data)
-  todoAdding.value = ""
-}
-function addingOnList(data) {
-  listgroup.innerHTML = ""
-  data.map((item, index) => {
-    listgroup.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between gap-3">${item}
-          <div class="lists d-flex align-items-center gap-3">
-            <a href="" class="confirm-item">
-            <?xml version="1.0" ?><svg class="feather feather-edit" fill="none" height="14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="14" xmlns="http://www.w3.org/2000/svg"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
-            </a>
-            <a href="#" class="delete-item">
-              <i id="deleteItem" class="fa fa-remove rovshan"></i> 
-            </a>
-          </div>
-        </li>
-      `
-    const deleteButton = listgroup.querySelector(".delete-item");
-    deleteButton.addEventListener("click", () => {
-      getDelete(index);
-    });
-  })
-}
-const li = document.querySelectorAll(".list-group-item");
-li.forEach((item) => {
-  item.addEventListener("dblclick", () => {
-    item.style.background = "green";
-    item.style.fontStyle = "italic";
-  });
-});
-searching.addEventListener("submit", getFind)
-function getFind(e) {
-  e.preventDefault()
-  const searchTerm = searching.value.trim()
-  if (data.includes(searchTerm)) {
-    const newData = []
-    newData.push(searchTerm)
-    addingOnList(newData)
+  let data = JSON.parse(localStorage.getItem("todo")) || []
+  if (!todoAdding.value.trim()) {
+    alert("bir sey yaz ")
+  } else {
+    data.push(todoAdding.value)
+
   }
-  searching.value = ""
+  localStorage.setItem("todo", JSON.stringify(data))
+  todoAdding.value = ""
+  toDoListVisible()
 }
 
+// yaradilmis todo listesinin goruntusu
+function toDoListVisible() {
+  listgroup.innerHTML = ""
+  const todo = JSON.parse(localStorage.getItem("todo")) || []
 
-function getDelete(index) {
-  data.splice(index, 1);
-  localStorage.setItem('tasks',JSON.stringify(data))
-  addingOnList(data);
+  if (todo != []) {
+    todo.forEach((item, index) => {
+      listgroup.innerHTML += `
+        <li ondblclick="getComplete('${item}')" class="list-group-item d-flex justify-content-between">${item}
+        <div class="sala d-flex  align-items-center">
+          <svg class="editing" onclick="(getModal('${item}',${index}))" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+         </svg>
+           <a href="#" onclick="removeItem(${index})"class="delete-item mx-3">
+             <i class="fa fa-remove rovshan"></i>
+           </a>
+            </div>
+         </li>
+       `
+    })
+  }
+}
+// statistika
+
+
+// tamamlamaq ucun
+function getComplete(item) {
+  item.style.color = "red"
+  toDoListVisible()
+}
+// elave edilen bir todo'nu silmek ucun
+function removeItem(index) {
+  let todo = JSON.parse(localStorage.getItem("todo")) || [];
+  todo.splice(index, 1)
+  localStorage.setItem("todo", JSON.stringify(todo))
+  toDoListVisible()
 }
 
+// elave edilen butun todolari silmek ucun
 document.getElementById("todoClearButton").addEventListener("click", () => {
-  data = [];
-  addingOnList(data)
+  localStorage.clear("todo")
+  toDoListVisible()
 })
+
+// her hansi bir todonu redakte etmek ucun
+let editModal = document.getElementById("modal-special")
+function getModal(item, index) {
+  editModal.innerHTML = ""
+  editModal.innerHTML = `
+  <div id="modalDiv">
+        <i onclick="closedModal()" class="close fa-solid fa-xmark"></i>
+        <input id="newValue" type="text" value="${item}">
+        <button id="saveBttn" onclick="savedValue('${index}')">Save</button>
+    </div>
+  `
+  editModal.style.display = "flex"
+}
+// bir yazi daxil edilmediyinde error versin  
+function alerts() {
+  editModal.innerHTML = ""
+  editModal.innerHTML = `
+  <div class="alert container d-flex align-items-center justify-content-around">
+        <p class="m-0">Zehmet olmasa bir deyer daxil edin</p>
+        <i class="alertIcon fa-solid fa-triangle-exclamation"></i>
+    </div>
+  `
+  editModal.style.display = "flex"
+}
+function closedModal() {
+  editModal.style.display = "none"
+}
+function savedValue(index) {
+  let todo = JSON.parse(localStorage.getItem("todo")) || []
+  let newValues = document.getElementById("newValue").value
+  try {
+    if (!newValues.trim()) {
+      throw new Error(alerts())
+    } else {
+      todo.splice(index, 1, `${newValues}`)
+      localStorage.setItem("todo", JSON.stringify(todo))
+      toDoListVisible()
+      closedModal()
+    }
+  } catch (err) {
+    err.message
+  }
+
+}
+// search
+
+function searchAlert() {
+  editModal.innerHTML = `
+  <div class="alert container d-flex align-items-center justify-content-around">
+        <p class="m-0">Melumat tapilmadi</p>
+        <i class="alertIcon fa-solid fa-triangle-exclamation"></i>
+    </div>
+  `;
+  editModal.style.display = "flex";
+}
+searching.addEventListener("input", getSearch);
+function getSearch() {
+  let todo = JSON.parse(localStorage.getItem("todo")) || [];
+  let searchTerm = searching.value.toLowerCase();
+  let filterData = todo.filter(item => item.toLowerCase().includes(searchTerm));
+
+  if (filterData.length > 0) {
+    listgroup.innerHTML = filterData.map(item => `
+      <li class="list-group-item d-flex justify-content-between">${item}
+        <div class="d-flex align-items-center">
+          <svg class="editing" onclick="getModal('${item}')" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+          </svg>
+          <a href="#" onclick="removeItem(${todo.indexOf(item)})" class="delete-item mx-3">
+            <i class="fa fa-remove rovshan"></i>
+          </a>
+        </div>
+      </li>
+    `).join("");    
+    editModal.style.display = "none";
+  } else {
+    searchAlert();
+    listgroup.innerHTML = "";
+  }
+}
+// her zaman listin goruntusu olsun
+toDoListVisible()
